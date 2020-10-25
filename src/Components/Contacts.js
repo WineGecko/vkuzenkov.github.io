0d75b9877160d2ab81697942b79cf662203c6024
Viktor Kuzenkov <v.kuzenkov@fintechru.org>
2020-10-25 05:51:14 +0300

    Base structure

---
+++ src/Components/Contacts.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Contacts extends Component {
+  render() {
+    return (
+        <section id="contacts">
+            <h2>I'm component</h2>
+        </section>
+        );
+  }
+}
+export default Contacts;
